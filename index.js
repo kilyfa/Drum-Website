fac54d54e3ae3b031c3236bfc1f739be8cefa92a
kilyfa <122576090+kilyfa@users.noreply.github.com>
2024-01-29 15:07:26 +0700

    a

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function SoundsActive(name, assets) {
   });
 }
 
+function SoundsActiveNoClicks(name, assets) {
+  var audio = new Audio("sounds/" + assets + ".mp3");
+  audio.play();
+}
 SoundsActive(".w", "tom-1");
 SoundsActive(".a", "tom-2");
 SoundsActive(".s", "tom-3");
@@ -15,4 +19,28 @@ SoundsActive(".l", "crash");
 
 document.addEventListener("keypress", function (event) {
   console.log(event);
+  var number = event.key;
+  switch (number) {
+    case "w":
+      SoundsActiveNoClicks(".w", "tom-1");
+      break;
+    case "a":
+      SoundsActiveNoClicks(".a", "tom-2");
+      break;
+    case "s":
+      SoundsActiveNoClicks(".s", "tom-3");
+      break;
+    case "d":
+      SoundsActiveNoClicks(".d", "tom-4");
+      break;
+    case "j":
+      SoundsActiveNoClicks(".j", "snare");
+      break;
+    case "k":
+      SoundsActiveNoClicks(".k", "kick-bass");
+      break;
+    case "l":
+      SoundsActiveNoClicks(".l", "crash");
+      break;
+  }
 });
